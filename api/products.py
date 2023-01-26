bb07514c9a9f1370a465ab35786c484f5bdecb44
Ole J. Berg <55295828+ole-berg@users.noreply.github.com>
2023-01-26 20:50:48 +0100

    Add API to add products (#77)
    * read products from db and return json
    
    * Add MariaDB connector to api docker image to fix build
    
    * add product which is passed as json to db
    
    * SZK-54 error message corrected
    
    * change cat_name to category_name
    
    * remove /list and /add
    
    * Remove unused MariaDB dependency
    
    Co-authored-by: KingOfDog <logins@kingofdog.de>
    Co-authored-by: 0100 1011 0100 1111 0100 0100 <KingOfDog@users.noreply.github.com>

--- api/products.py
+++ api/products.py
@@ -1,10 +1,8 @@
-import pymysql
-import sys
 import os
-import simplejson as json
+import pymysql
 from dotenv import load_dotenv
-from flask import request, jsonify
 from flask import Blueprint
+from flask import request, jsonify
 
 load_dotenv()
 
@@ -24,7 +22,7 @@ def product_list():
     conn = pymysql.connect(**config)
     cur = conn.cursor()
 
-    query = "select p.id, p.name, p.price, p.description, p.image_url, c.name as cat_name FROM product p LEFT JOIN " \
+    query = "select p.id, p.name, p.price, p.description, p.image_url, c.name as category_name FROM product p LEFT JOIN " \
             "category c " \
             "ON p.category_id = c.id"
 
@@ -37,3 +35,38 @@ def product_list():
         json_data.append(dict(zip(row_headers, result)))
 
     return jsonify(json_data)
+
+
+@products.route('/api/v1/products', methods=['POST'])
+def add_product():
+    product_to_add = request.get_json(force=True)
+
+    if product_to_add['name'] == '':
+        return "Name missing!", 400
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+
+    check_category_query = "select id from category c where name = %s"
+    cur.execute(check_category_query, (product_to_add['category_name'],))
+    category_id = cur.fetchone()
+
+    if category_id is None:
+        add_category_sql = "insert into category (name) values (%s)"
+        cur.execute(add_category_sql, (product_to_add['category_name'],))
+        conn.commit()
+
+    cur.execute(check_category_query, (product_to_add['category_name'],))
+    category_id = cur.fetchone()
+
+    insert_product_sql = "insert into product (name, price, description, image_url, category_id) " \
+                         "values (%s, %s, %s, %s, %s)"
+    cur.execute(insert_product_sql,
+                (product_to_add['name'],
+                 float(product_to_add['price']),
+                 product_to_add['description'],
+                 product_to_add['image_url'],
+                 int(category_id[0]),))
+    conn.commit()
+
+    conn.close()
+    return "Product successfully added", 200
