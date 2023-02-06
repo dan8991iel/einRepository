15c24977123ba0cb8cf63509bc0eaed52568653a
dan8991iel <daniel.krasemann@web.de>
2023-02-06 01:49:28 +0100

    - update

--- client/src/components/PMProductEdit.vue
+++ client/src/components/PMProductEdit.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex w-full">
       <div class="mb-4">
-        <label class="block mb-2 font-medium text-gray-700" for="select_product"
-          >Select Product:</label
-        >
+        <label class="block mb-2 font-medium text-gray-700" for="select_product">
+          Select Product:
+        </label>
         <select
           class="w-full p-2 border border-gray-400 rounded-lg"
           v-model="selectedProduct"
@@ -14,9 +14,9 @@
       </div>
       <form @submit.prevent="editProduct" class="w-full p-6 bg-white rounded-lg">
         <div class="mb-4">
-          <label class="block mb-2 font-medium text-gray-700" for="name"
-            >Name:</label
-          >
+          <label class="block mb-2 font-medium text-gray-700" for="name">
+            Name:
+          </label>
           <input
             class="w-full p-2 border border-gray-400 rounded-lg"
             v-model="selectedProduct.name"
@@ -26,100 +26,88 @@
           />
         </div>
         <div class="mb-4">
-          <label class="block mb-2 font-medium text-gray-700" for="description">Description:</label>
-<textarea
-       class="w-full p-2 border border-gray-400 rounded-lg"
-       v-model="selectedProduct.description"
-       id="description"
-       required
-     ></textarea>
-</div>
-<div class="mb-4">
-<label class="block mb-2 font-medium text-gray-700" for="price"
-       >Price:</label
-     >
-<input
-       class="w-full p-2 border border-gray-400 rounded-lg"
-       v-model="selectedProduct.price"
-       type="number"
-       id="price"
-       required
-     />
-</div>
-<div class="mb-4">
-<label class="block mb-2 font-medium text-gray-700" for="stock"
-       >Stock:</label
-     >
-<input
-       class="w-full p-2 border border-gray-400 rounded-lg"
-       v-model="selectedProduct.stock"
-       type="number"
-       id="stock"
-       required
-     />
-</div>
-<div class="mb-4">
-<label class="block mb-2 font-medium text-gray-700" for="category"
-       >Category:</label
-     >
-<input
-       class="w-full p-2 border border-gray-400 rounded-lg"
-       v-model="selectedProduct.category"
-       type="text"
-       id="category"
-       required
-     />
-</div>
-<div class="flex items-center justify-end">
-<button
-       class="bg-indigo-500 text-white p-2 rounded-lg hover:bg-indigo-600"
-       type="submit"
-     >
-Edit Product
-</button>
-</div>
-</form>
+          <label class="block mb-2 font-medium text-gray-700" for="price">
+            Price:
+          </label>
+          <input
+            class="w-full p-2 border border-gray-400 rounded-lg"
+            v-model="selectedProduct.price"
+            type="number"
+            id="price"
+            required
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block mb-2 font-medium text-gray-700" for="description">
+            Description:
+          </label>
+          <textarea
+            class="w-full p-2 border border-gray-400 rounded-lg"
+            v-model="selectedProduct.description"
+            id="description"
+            required
+          ></textarea>
+        </div>
+        <div class="mb-4">
+          <label class="block mb-2 font-medium text-gray-700" for="category">
+            Category:
+          </label>
+          <input
+            class="w-full p-2 border border-gray-400 rounded-lg"
+            v-model="selectedProduct.category_name"
+            type="text"
+            id="category"
+            required
+          />
+        </div>
+        <div class="flex items-center justify-end">
+          <button
+            class="bg-indigo-500 text-white p-2 rounded-lg hover:bg-indigo-600"
+            type="submit"
+          >
+            Edit Product
+          </button>
+        </div>
+      </form>
+    </div>
+  </template>
+<script setup lang="ts">
+import { ref } from "vue";
+import type { Ref } from "vue";
+import type { Product } from "@/models/Product";
 
-  </div>
-</template>
-<script>
-export default {
-  data() {
-    return {
-      selectedProduct: {},
-      products: [
-        {
-          id: 1,
-          name: "Product 1",
-          description: "This is a sample product",
-          price: 9.99,
-          stock: 10,
-          category: "Category 1"
-        },
-        {
-          id: 2,
-          name: "Product 2",
-          description: "This is another sample product",
-          price: 19.99,
-          stock: 20,
-          category: "Category 2"
-        },
-        {
-          id: 3,
-          name: "Product 3",
-          description: "This is yet another sample product",
-          price: 29.99,
-          stock: 30,
-          category: "Category 3"
-        }
-      ]
-    };
-  },
-  methods: {
-    editProduct() {
-      // Add logic for editing product
-      console.log("Edited Product: ", this.selectedProduct);
+const selectedProduct: Ref<Product> = ref({} as Product);
+const products: Product[] = [
+    {
+    id: 1,
+    name: "Product 1",
+    price: 9.99,
+    description: "This is a sample product",
+    category_name: "Category 1",
+    image_url: ""
+    },
+    {
+    id: 2,
+    name: "Product 2",
+    price: 9.99,
+    description: "This is a sample product",
+    category_name: "Category 1",
+    image_url: ""
+    },
+    {
+    id: 3,
+    name: "Product 3",
+    price: 9.99,
+    description: "This is a sample product",
+    category_name: "Category 1",
+    image_url: ""
     }
-  }
-};
+];
+
+
+
+function editProduct() {
+    console.log("Edited Product: ", selectedProduct.value);
+}
+
 </script>
