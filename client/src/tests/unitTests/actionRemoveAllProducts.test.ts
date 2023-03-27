0228f8b8f60b690e23292c62337a037a2f222064
Judith2305 <bakesbyjudi@gmail.com>
2023-03-27 15:40:53 +0200

    UnitTest RemoveAllProductsFromCart

--- client/src/tests/unitTests/actionRemoveAllProducts.test.ts
+++ client/src/tests/unitTests/actionRemoveAllProducts.test.ts
@@ -1,1 +1,21 @@
 import {useCartStore} from '../../stores/cart';
+import { createPinia, setActivePinia } from 'pinia';
+import {test, describe, expect, beforeEach} from 'vitest';
+import type { Product } from '@/models/Product';
+import { CartItem } from '@/models/CartItem';
+
+describe("removeFromCart",()=>{
+
+    beforeEach(() => {
+        setActivePinia(createPinia())
+      })
+
+    test("remove product from cart",()=>{
+        
+        const store = useCartStore();
+        const product:Product = {id:1, name:"Keks", price:20.00, description:"Schoko", category_name:"Lebensmittel", image_url:"https://image.essen-und-trinken.de/13073754/t/ld/v3/w960/r1/-/schokokekse-adobestock-245097665.jpg"};
+        store.add(product);
+        store.removeAllOfProduct(product);
+        expect(Array.from (store.items.values())).not.toContainEqual(product);
+    })
+})
