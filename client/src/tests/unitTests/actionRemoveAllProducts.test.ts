c46ef60abf95c27fa3c0bd52e28a69293801bbde
Judith2305 <bakesbyjudi@gmail.com>
2023-03-27 15:47:04 +0200

    Fix Test: RemoveAllProducts

--- client/src/tests/unitTests/actionRemoveAllProducts.test.ts
+++ client/src/tests/unitTests/actionRemoveAllProducts.test.ts
@@ -16,6 +16,6 @@ describe("removeFromCart",()=>{
         const product:Product = {id:1, name:"Keks", price:20.00, description:"Schoko", category_name:"Lebensmittel", image_url:"https://image.essen-und-trinken.de/13073754/t/ld/v3/w960/r1/-/schokokekse-adobestock-245097665.jpg"};
         store.add(product);
         store.removeAllOfProduct(product);
-        expect(Array.from (store.items.values())).not.toContainEqual(product);
+        expect(Array.from (store.items.values())).not.toContainEqual(new CartItem(product, 1));
     })
 })
